43e0d9fcbe9b302df013520f490c912b883993c7
Ahmed Rahi <arahi@fordham.edu>
2023-02-13 09:51:52 -0500

    changes

--- data-sets/prompt4/superheroes.js
+++ data-sets/prompt4/superheroes.js
@@ -37,6 +37,41 @@ const superheroSchema = new mongoose.Schema({
       ],
     },
   ],
+  universe: String,
+  movies: [
+    {
+      title: String,
+      year: Date,
+      characters: [
+        {
+          name: String,
+          alias: Boolean,
+          actor: String,
+        },
+        {
+          name: String,
+          alias: Boolean,
+          actor: String,
+        },
+      ],
+    },
+    {
+      title: String,
+      year: Date,
+      characters: [
+        {
+          name: String,
+          alias: Boolean,
+          actor: String,
+        },
+        {
+          name: String,
+          alias: Boolean,
+          actor: String,
+        },
+      ],
+    },
+  ],
 });
 
 const Superhero = mongoose.model("Superhero", superheroSchema);
